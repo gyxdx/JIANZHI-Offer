9a4256f883135b10da21e5bb8ede07a427cfcbde
gyxmm <32378935+gyxmm@users.noreply.github.com>
2018-12-25 21:17:32 +0800

    Create  printListFromTailToHead.java

---
+++ printListFromTailToHead.java
@@ -0,0 +1,25 @@
+
+
+import java.util.*;
+import java.util.ArrayList;
+public class Solution {
+	public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+		Stack<Integer> stack = new Stack<>();
+		while (listNode != null) {
+			stack.add(listNode.val);
+			listNode = listNode.next;
+		}
+		ArrayList<Integer> ret = new ArrayList<>();
+		while (!stack.isEmpty())
+			ret.add(stack.pop());
+		return ret;
+	}
+
+}
+class ListNode {
+	int val;
+	ListNode next = null;
+	ListNode(int val) {
+		this.val = val;
+	}
+}
